8073aae405499e98da18367c4519b9ffae5186aa
Dev Random <dev@rndm>
2022-01-11 14:32:07 +0100

    mock out parser functions for offline data and url

--- cmd/verify/verify.go
+++ cmd/verify/verify.go
@@ -3,11 +3,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
 	"log"
 	"net/url"
+	"os"
+	"strings"
 )
 
 var (
@@ -27,17 +29,24 @@ func main() {
 		os.Exit(-1)
 	}
 
-	// a dataFile is specified
 	if *dataFile != "" {
-		//verifyData(*dataFile)
-		panic("NotImplemented")
-	// construct a url from the endpoint and page name
+		// a dataFile is specified
+		if verifyData(*dataFile) {
+			fmt.Println("Verified:", *dataFile)
+		} else {
+			fmt.Println("Failed to verify:", *dataFile)
+		}
 	} else {
+		// else construct a url from the endpoint and page name
 		u, err := url.Parse(*endpoint + "/" + flag.Args()[0])
 		if err != nil {
 			log.Fatalln(err)
 		}
-		//verifyURL(u)
+		if verifyURL(u) {
+			fmt.Println("Verified:", u)
+		} else {
+			fmt.Println("Failed to verify:", u)
+		}
 	}
 }
 
@@ -45,4 +54,45 @@ func usage() {
 	fmt.Println("Usage:\n", "verify [OPTIONS] <page title>\n",
 		"or\n", "verify [OPTIONS] --file <offline file.json or file.xml>\n")
 	flag.Usage()
+}
+
+type offlineData struct {
+	pages []pageData
+}
+
+type pageData struct {
+	title string
+	body  string
+}
+
+func verifyData(fileName string) bool {
+	_, err := readExportFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return false
+}
+
+func verifyURL(u *url.URL) bool {
+	return false
+}
+
+func readExportFile(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if strings.HasSuffix(strings.ToLower(f.Name()), ".json") {
+		d := json.NewDecoder(f)
+		data := &offlineData{}
+		err := d.Decode(data)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if strings.HasSuffix(strings.ToLower(f.Name()), ".xml") {
+		panic("NotImplemented")
+	}
+	return nil, nil
 }
