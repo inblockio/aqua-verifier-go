d64132a86fad70faf81a2ba57ebd60e735cb065a
Dev Random <dev@rndm>
2022-01-15 03:31:47 +0100

    add Makefile and wasm tests
    TODO: instrument the tests using nodejs

---
+++ cmd/verify/verify_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestWasm(t *testing.T) {
+	// Spawns a http server and browser to test the javascript (wasm) build of verify loads in console
+	// TODO: can automate using nodejs using go_js_wasm_exec, e.g.
+	// GOOS=js GOARCH=wasm go test -exec="$(go env GOROOT)/misc/wasm/go_js_wasm_exec"
+	// TODO: load json into DOM and verify
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: http.FileServer(http.Dir(`.`)),
+	}
+	go func() {
+		s.ListenAndServe()
+	}()
+
+	// launch brower, wait for exit
+	b := exec.Command("xdg-open", "http://localhost:8080")
+	_, err := b.Output()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	p := b.Process.Pid
+	proc, err := os.FindProcess(p)
+	if err == nil {
+		proc.Wait()
+	}
+	s.Close()
+}
