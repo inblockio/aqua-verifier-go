aed7665eb68f69f8aea56a5ac99679da49962385
rht <rhtbot@protonmail.com>
2022-03-02 02:25:28 -0500

    test: 💍 Add all tests for verifyRevision except for file ver

--- verify/verify_test.go
+++ verify/verify_test.go
@@ -39,22 +39,31 @@ func getFixtureVerificationSet() ([]*api.Revision, error) {
 	return verificationSet, err
 }
 
-func getFirstFixtureVS() (*api.Revision, error) {
+func get1st2ndFixtureVerStructure() (*api.Revision, *api.Revision, error) {
+	// Return the first and second revision
 	verificationSet, err := getFixtureVerificationSet()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	first := verificationSet[0]
-	return first, nil
+	second := verificationSet[1]
+	return first, second, nil
 }
 
 func verifyFirstRevision(first *api.Revision, args ...bool) (bool, *RevisionVerificationResult) {
-	doVerifyMerkleProof := true
+	doVerifyMerkleProof := false
 	if len(args) > 0 {
 		doVerifyMerkleProof = args[0]
 	}
 	isCorrect, result := verifyRevision(first, nil, doVerifyMerkleProof)
 	return isCorrect, result
+}
+
+func expectErrorFirstRevision(require *require.Assertions, first *api.Revision, errMsg string) *RevisionVerificationResult {
+	isCorrect, result := verifyFirstRevision(first)
+	require.False(isCorrect)
+	require.EqualError(result.Error, errMsg)
+	return result
 }
 
 func TestVerifyRevision(t *testing.T) {
@@ -85,45 +94,99 @@ func TestVerifyRevision(t *testing.T) {
 func TestInvalidContent(t *testing.T) {
 	// When the content is tampered
 	require := require.New(t)
-	first, err := getFirstFixtureVS()
+	first, _, err := get1st2ndFixtureVerStructure()
 	require.NoError(err)
 	first.Content.Content["main"] = "wrong"
-	isCorrect, result := verifyFirstRevision(first)
-	require.False(isCorrect)
-	require.EqualError(result.Error, "Content hash doesn't match")
+	expectErrorFirstRevision(require, first, "Content hash doesn't match")
 }
 
 func TestInvalidMetadata(t *testing.T) {
 	// When the metadata is tampered
 	require := require.New(t)
-	first, err := getFirstFixtureVS()
+	first, _, err := get1st2ndFixtureVerStructure()
 	require.NoError(err)
 	first.Metadata.DomainId = "wrong"
-	isCorrect, result := verifyFirstRevision(first)
-	require.False(isCorrect)
-	require.EqualError(result.Error, "Metadata hash doesn't match")
+	expectErrorFirstRevision(require, first, "Metadata hash doesn't match")
 }
+
 func TestInvalidPreviousSignature(t *testing.T) {
 	// When previous signature is tampered
 	require := require.New(t)
-	doVerifyMerkleProof := true
-	verificationSet, err := getFixtureVerificationSet()
+	doVerifyMerkleProof := false
+	first, second, err := get1st2ndFixtureVerStructure()
 	require.NoError(err)
-	first := verificationSet[0]
-	second := verificationSet[1]
+
 	first.Signature.Signature = "wrong"
 	isCorrect, result := verifyRevision(second, first, doVerifyMerkleProof)
 	require.False(isCorrect)
 	require.EqualError(result.Error, "Previous signature hash doesn't match")
+
+	// A different error message for when the prev revision is nil
+	isCorrect, result = verifyRevision(second, nil, doVerifyMerkleProof)
+	require.False(isCorrect)
+	require.EqualError(result.Error, "Revision has previous signature, but no previous revision provided to validate")
+}
+
+func TestInvalidPreviousWitness(t *testing.T) {
+	// When previous witness is tampered
+	require := require.New(t)
+	doVerifyMerkleProof := false
+	first, second, err := get1st2ndFixtureVerStructure()
+	require.NoError(err)
+
+	first.Witness.MerkleRoot = "wrong"
+	isCorrect, result := verifyRevision(second, first, doVerifyMerkleProof)
+	require.False(isCorrect)
+	require.EqualError(result.Error, "Previous witness hash doesn't match")
+
+	// When the prev revision is set to nil when it is not supposed to
+	first.Witness = nil
+	isCorrect, result = verifyRevision(second, first, doVerifyMerkleProof)
+	require.False(isCorrect)
+	require.EqualError(result.Error, "Previous witness data not found")
+}
+
+func TestInvalidCurrentWitness(t *testing.T) {
+	// When current witness is tampered
+	require := require.New(t)
+	doVerifyMerkleProof := true
+	first, _, err := get1st2ndFixtureVerStructure()
+	require.NoError(err)
+
+	oldMR := first.Witness.MerkleRoot
+	first.Witness.MerkleRoot = "wrong"
+	isCorrect, result := verifyRevision(first, nil, doVerifyMerkleProof)
+	require.False(isCorrect)
+	require.Equal(result.Status.Witness, "INVALID")
+	// Reset the merkle root to the correct one again, to prepare for the next
+	// test.
+	first.Witness.MerkleRoot = oldMR
+
+	// When the Merkle proof is tampered
+	first.Witness.MerkleProof[0].LeftLeaf = "wrong"
+	isCorrect, result = verifyRevision(first, nil, doVerifyMerkleProof)
+	require.False(isCorrect)
+	require.Equal(result.WitnessResult.MerkleProofStatus, "INVALID")
 }
 
 func TestInvalidCurrentSignature(t *testing.T) {
 	// When current signature is tampered
 	require := require.New(t)
-	first, err := getFirstFixtureVS()
+	first, _, err := get1st2ndFixtureVerStructure()
 	require.NoError(err)
 	first.Signature.Signature = "wrong"
 	isCorrect, result := verifyFirstRevision(first)
 	require.False(isCorrect)
 	require.Equal(result.Status.Signature, "INVALID")
+}
+
+func TestInvalidVerificationHash(t *testing.T) {
+	// When the verification hash is tampered
+	require := require.New(t)
+	first, _, err := get1st2ndFixtureVerStructure()
+	require.NoError(err)
+	first.Metadata.VerificationHash = "wrong"
+	isCorrect, result := verifyFirstRevision(first)
+	require.False(isCorrect)
+	require.Equal(result.Status.Verification, "INVALID")
 }
